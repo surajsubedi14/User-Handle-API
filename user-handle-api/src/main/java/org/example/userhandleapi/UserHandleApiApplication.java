6ec9563d4014421928f27fdcf8d8fa26b4271de3
surajsubedi14 <surajsubedi14@gmail.com>
2024-03-19 14:58:27 +0530

    initial commit

---
+++ user-handle-api/src/main/java/org/example/userhandleapi/UserHandleApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.userhandleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserHandleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserHandleApiApplication.class, args);
+    }
+
+}
