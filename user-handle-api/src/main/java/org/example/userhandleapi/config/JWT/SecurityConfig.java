4f8be331dead5624025e5db4e9a5d312d5fb78a1
Rishav Chandel <rishavchandel456@gmail.com>
2024-04-14 13:15:43 +0530

    updated

--- user-handle-api/src/main/java/org/example/userhandleapi/config/JWT/SecurityConfig.java
+++ user-handle-api/src/main/java/org/example/userhandleapi/config/JWT/SecurityConfig.java
@@ -99,7 +99,7 @@ public class SecurityConfig {
         corsConfiguration.setAllowedMethods(List.of("*"));
         corsConfiguration.addAllowedHeader("*");
 
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001","http://localhost:3002","http://localhost:3003","http://localhost:3004","http://localhost:3005","http://localhost:8080"));
         corsConfiguration.setAllowedHeaders(List.of("Content-Type","text/plain","Authorization"));
 
         var source = new UrlBasedCorsConfigurationSource();
